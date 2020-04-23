1b469c1a0df77a782457c0e6c2ff0c422202ff51
SylvainG <sguerrin549@gmail.com>
2020-04-23 16:30:59 +0200

    finalize cases studies

--- web/app/themes/maquette/index.php
+++ web/app/themes/maquette/index.php
@@ -2,5 +2,7 @@
 
 $context = \Timber\Timber::context();
 $context['post'] = new \Timber\Post();
+//$context['term'] = new \Timber\Term();
+//$context['term_page'] = new \Timber\Term();
 
 \Timber\Timber::render('index.twig', $context);
